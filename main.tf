9c21348b691195278750b80708224b66048b4dbd
Pubudu Perera <pubudu@wego.com>
2020-05-25 15:57:43 +0800

    Removed vpc module

--- main.tf
+++ main.tf
@@ -125,27 +125,6 @@ resource "aws_ssm_parameter" "atlantis_bitbucket_user_token" {
   value = "${var.atlantis_bitbucket_user_token}"
 }
 
-###################
-# VPC
-###################
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "v1.53.0"
-
-  create_vpc = "${var.vpc_id == ""}"
-
-  name = "${var.name}"
-
-  cidr            = "${var.cidr}"
-  azs             = "${var.azs}"
-  private_subnets = "${var.private_subnets}"
-  public_subnets  = "${var.public_subnets}"
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  tags = "${local.tags}"
-}
 
 ###################
 # ALB
